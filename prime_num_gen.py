b0c60d75d773ca0fbb3f0b2c05dd0d264436013a
lizkarimi <lizkarimi1997@gmail.com>
2017-04-05 00:19:25 +0300

    prime_num_gen.py
    final commit

--- prime_num_gen.py
+++ prime_num_gen.py
@@ -9,4 +9,12 @@ def prime_num_gen(n):
 def check_if_prime(n):
 	if (n==2) or (n==3):
 		return True
-	
+	if n<=1:
+		retun False
+	for i in range(3,n):
+		if (n%2 == 0) or (n%3 == 0) or (n%1 == 0):
+			return False
+		else:
+			return True
+			
+print(prime_num_gen(20))
